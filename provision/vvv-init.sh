dd45bbd71a578d647bc2ee7561a4d65790ddcfad
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2017-12-29 14:21:25 -0500

    updated for more common usage

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-# Provision WordPress Stable
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database 'wordpress_default' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_default"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_default.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\nCreating database '${VVV_SITE_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${VVV_SITE_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${VVV_SITE_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
@@ -13,34 +12,11 @@ touch ${VVV_PATH_TO_SITE}/log/error.log
 touch ${VVV_PATH_TO_SITE}/log/access.log
 
 # Install and configure the latest stable version of WordPress
-if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
-
-  echo "Downloading WordPress Stable, see http://wordpress.org/"
-  cd ${VVV_PATH_TO_SITE}
-  curl -L -O "https://wordpress.org/latest.tar.gz"
-  noroot tar -xvf latest.tar.gz
-  mv wordpress public_html
-  rm latest.tar.gz
-  cd ${VVV_PATH_TO_SITE}/public_html
-
-  echo "Configuring WordPress Stable..."
-  noroot wp core config --dbname=wordpress_default --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-// Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(local.wordpress.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
-    define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
-    define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
-}
-
-define( 'WP_DEBUG', true );
-PHP
-
-  echo "Installing WordPress Stable..."
-  noroot wp core install --url=local.wordpress.test --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
-
+cd ${VVV_PATH_TO_SITE}
+if ! $(wp core is-installed --allow-root); then
+  wp core download --path="${VVV_PATH_TO_SITE}" --allow-root
+  wp core config --dbname="${VVV_SITE_NAME}" --dbuser=wp --dbpass=wp --quiet --allow-root
+  wp core install --url="${VVV_SITE_NAME}.test" --quiet --title="${VVV_SITE_NAME}" --admin_name=admin --admin_email="admin@${VVV_SITE_NAME}.test" --admin_password="password" --allow-root
 else
-
-  echo "Updating WordPress Stable..."
-  cd ${VVV_PATH_TO_SITE}/public_html
-  noroot wp core update
-
+  wp core update --allow-root
 fi
