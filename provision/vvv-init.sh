464fa05b2eabc1acd368a8097abfd4b3ac90dedc
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2017-12-29 14:34:51 -0500

    hardcode db name

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -2,8 +2,8 @@
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${VVV_SITE_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${VVV_SITE_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${VVV_SITE_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS workshopdigital"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON workshopdigital.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
@@ -15,7 +15,7 @@ touch ${VVV_PATH_TO_SITE}/log/access.log
 cd ${VVV_PATH_TO_SITE}
 if ! $(wp core is-installed --allow-root); then
   wp core download --path="${VVV_PATH_TO_SITE}" --allow-root
-  wp core config --dbname="${VVV_SITE_NAME}" --dbuser=wp --dbpass=wp --quiet --allow-root
+  wp core config --dbname="workshopdigital" --dbuser=wp --dbpass=wp --quiet --allow-root
   wp core install --url="${VVV_SITE_NAME}.test" --quiet --title="${VVV_SITE_NAME}" --admin_name=admin --admin_email="admin@${VVV_SITE_NAME}.test" --admin_password="password" --allow-root
 else
   wp core update --allow-root
